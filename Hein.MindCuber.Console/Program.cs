951b8ac3ef23f0ec5ba9b34392412e2f59e0d5ec
Hein Pauwelyn <hein.pauwelyn@savaco.com>
2020-01-04 13:24:39 +0100

    startup

---
+++ Hein.MindCuber.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hein.MindCuber.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
