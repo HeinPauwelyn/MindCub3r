b810250dbd0ed963ae660f6a368899e376fa1079
Hein Pauwelyn <hein.pauwelyn@savaco.com>
2020-01-04 16:12:13 +0100

    turn motor and get distance

--- Hein.MindCuber.Console/Program.cs
+++ Hein.MindCuber.Console/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+using System;
 
 namespace Hein.MindCuber.Console
 {
     class Program
     {
+        private static Brick _brick;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Connect();
+        }
+
+        private async static void Connect()
+        {
+            _brick = new Brick(new BluetoothCommunication("COM10"));
+            _brick.BrickChanged += BrickChanged;
+            await _brick.ConnectAsync();
+            await _brick.DirectCommand.PlayToneAsync(100, 3000, 100);
+        }
+
+        private static void BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            System.Console.WriteLine("Brick changed");
         }
     }
 }
---
+++ Hein.MindCuber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+using static System.Console;
+
+namespace Hein.MindCuber.Console
+{
+    class Program
+    {
+        private static Brick _brick;
+
+        static void Main(string[] args)
+        {
+            Connect();
+            Reset();
+
+            ReadLine();
+        }
+
+        private async static void Reset()
+        {
+            await _brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.B, 40, 3000, false);
+        }
+
+        private async static void Connect()
+        {
+            _brick = new Brick(new UsbCommunication());
+            _brick.BrickChanged += BrickChanged;
+            await _brick.ConnectAsync();
+            await _brick.DirectCommand.PlayToneAsync(100, 3000, 100);
+        }
+
+        private static void BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            float distance = e.Ports[InputPort.Four].SIValue;
+
+            if (distance <= 20)
+            {
+                WriteLine($"start: {distance}");
+            }
+            else
+            {
+                WriteLine($"nope : {distance}");
+            }
+        }
+    }
+}
